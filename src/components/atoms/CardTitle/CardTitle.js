8db5971a94f85e458688cfeb758f7f13e75c374b
Damian Suwala <damian.suwala@gmail.com>
2021-07-22 21:38:10 +0200

    fix cardTitle style

--- src/components/atoms/CardTitle/CardTitle.js
+++ src/components/atoms/CardTitle/CardTitle.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const CardTitle = styled.a`
   font-size: ${({ theme }) => theme.fonts.l};
   color: ${({ theme }) => theme.colors.main};
-  font-weight: 600;
   cursor: pointer;
   text-decoration: none;
   transition: color 0.2s ease;
