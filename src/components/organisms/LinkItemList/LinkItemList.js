e7cddd631a98e27808365b36655fee75a1b43623
Damian Suwala <damian.suwala@gmail.com>
2021-10-03 13:22:47 +0200

    intersection observer hook added

--- src/components/organisms/LinkItemList/LinkItemList.js
+++ src/components/organisms/LinkItemList/LinkItemList.js
@@ -1,37 +1,19 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import LinkItem from 'components/molecules/LinkItem/LinkItem';
 import useGetLinks from 'hooks/useGetLinks';
 import { LoaderSpinner } from 'components/atoms/LoaderSpinner/LoaderSpinner';
+import useIntersectionObserver from 'hooks/useIntersectionObserver';
 
 export const LinkItemList = () => {
   const [lastItem, setLastItem] = useState(0);
   const { loading, error, links, nextPage } = useGetLinks(lastItem);
-  const observer = useRef(null);
-  const lastLinkRef = useRef(null);
 
   const getMoreLinks = useCallback(() => {
     if (loading || !nextPage) return;
     setLastItem(links[links.length - 1]);
   }, [links, loading, nextPage]);
 
-  useEffect(() => {
-    const options = {
-      root: document,
-      threshold: 1,
-    };
-    const callback = (entries) => {
-      if (entries[0].isIntersecting) {
-        getMoreLinks();
-      }
-    };
-    observer.current = new IntersectionObserver(callback, options);
-    if (lastLinkRef.current) {
-      observer.current.observe(lastLinkRef.current);
-    }
-    return () => {
-      observer.current.disconnect();
-    };
-  }, [getMoreLinks]);
+  const { lastLinkRef } = useIntersectionObserver(getMoreLinks);
 
   return (
     <>
